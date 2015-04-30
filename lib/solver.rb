c0a536f2087e58152d5870b31e79c5fac3b3ae8a
Andrew Fink <andrewfink8@gmail.com>
2015-04-30 15:45:20 -0600

    got the board made

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,11 +1,63 @@
+require 'pry'
+
+
 class Solver
 
-def initialize(puzzle_text)
-  @puzzle_text = puzzle_text
-end
+
+  LETTERMAP = ("a".."i").to_a
+
+  def initialize(puzzle_text)
+    @puzzle_text = puzzle_text
+
+  end
+
+
+  def split_data
+    @puzzle_text.split("\n").map(&:chars).flatten
+  end
+
+
+  def squares
+    first = ["z","w","t"]
+    second = ["y","v","s"]
+    third = ["x","u","r"]
+
+    squares = []
+    3.times do
+
+
+    3.times do
+      squares << [first[0], first[0], first[0]];
+      squares << [second[0], second[0], second[0]];
+      squares << [third[0], third[0], third[0]];
+    end
+
+    first.rotate!
+    second.rotate!
+    third.rotate!
+    end
+    squares.flatten
+  end
+
+
+  def make_board
+    letters = ("a".."i").to_a
+    row = ["x", "x", "x", "x", "x", "x", "x", "x", "x"]
+    letters_to_sub = letters.zip(row).map(&:join)
+    positions = []
+    9.times do |time|
+      positions << letters_to_sub.map do |pair|
+        pair.gsub('x', (time+1).to_s)
+      end
+    end
+    final_positions = positions.flatten.zip(squares).map(&:join)
+    final_positions.flatten.zip(split_data).to_h
+  end
+
 
   def output_data
-    p @puzzle_text
+    @puzzle_text
+    LETTERMAP
   end
 
   # parse into row, column, square
@@ -20,3 +72,43 @@ end
 
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# def make_board
+#   letters = ("a".."i").to_a
+#   numbers = (1..9).to_a
+#   board = []
+#   index_counter = 0
+#   9.times do
+#     x = letters[index_counter]
+#     index_counter += 1
+#     row = [x,x,x,x,x,x,x,x,x]
+#     row.zip(numbers)
+#     board << row
+#   end
+#   p board
+# end
