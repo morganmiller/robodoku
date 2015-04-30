e74242907d34408153fc3e8f3ccfeba39e69d395
Andrew Fink <andrewfink8@gmail.com>
2015-04-30 13:52:54 -0600

    starting robodoku

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,3 +1,22 @@
 class Solver
-  # your excellent code here
+
+def initialize(puzzle_text)
+  @puzzle_text = puzzle_text
+end
+
+  def output_data
+    p @puzzle_text
+  end
+
+  # parse into row, column, square
+
+  # coordinate system with a hash.... A1, A2, A3... to map a board
+
+  # know the value of a spot and/or if a spot is blank or not
+  # know where each spot is on the board
+  # find all of a spot's peers...
+  # check to see if there is an easy solution
+  # if not move on to check next open spot
+
+
 end
--- test/solver_test.rb
+++ test/solver_test.rb
@@ -1,8 +1,24 @@
 require "minitest"
 require "minitest/autorun"
+require "/lib/solver"
+require "/lib/sudoku"
 
 class SolverTest < Minitest::Test
+
+  def setup
+
+    puzzle_text = File.read("/puzzles/easy_sample.txt")
+    solver = Solver.new(puzzle_text)
+  end
+
   def test_it_solves
     assert false, "make it solve!"
   end
+
+  def test_it_can_output_puzzle_text
+    solver = Solver.new
+  end
+
+
+  # validation for solutions --- check row, column, and square
 end
