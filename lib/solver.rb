f32eca9a5f6cddba1ce476034d8a8e2fd555e5bd
Morgan Miller <m.miller722@gmail.com>
2015-05-02 17:53:52 -0600

    solver not solving!

--- lib/solver.rb
+++ lib/solver.rb
@@ -3,15 +3,45 @@ require 'pry'
 
 class Solver
 
-
-  LETTERMAP = ("a".."i").to_a
+  attr_reader :board
 
   def initialize(puzzle_text)
     @puzzle_text = puzzle_text
+    @board = {}
   end
 
   def split_data
-    @puzzle_text.split("\n").map(&:chars).flatten
+    @puzzle_text.split("\n").map(&:chars).flatten.map(&:to_i)
+  end
+
+  def columns
+    ("a".."i").to_a
+  end
+
+  def rows
+    ("1".."9").to_a
+  end
+
+  def squares
+    ("r".."z").to_a
+  end
+
+  def to_be_replaced_by_rows
+    ["x"] * 9
+  end
+
+  def to_sub_with_numbers
+    columns.zip(to_be_replaced_by_rows).map(&:join)
+  end
+
+  def columns_and_rows
+    positions = []
+    9.times do |time|
+      positions << to_sub_with_numbers.map do |pair|
+        pair.gsub('x', (time+1).to_s)
+      end
+    end
+    positions
   end
 
   def shovel_squares(squares, first, second, third)
@@ -34,33 +64,54 @@ class Solver
     squares.flatten
   end
 
+  def final_designators
+    columns_and_rows.flatten.zip(square_assignments).map(&:join)
+  end
 
-  def board
-    letters = ("a".."i").to_a
-    row = ["x"] * 9
-    letters_to_sub = letters.zip(row).map(&:join)
-    positions = []
-    9.times do |time|
-      positions << letters_to_sub.map do |pair|
-        pair.gsub('x', (time+1).to_s)
+  def make_board
+    @board = final_designators.flatten.zip(split_data).to_h
+  end
+
+  def all_numbers
+    (1..9).to_a
+  end
+
+  def find_peers_and_sub
+    #needs to check for keys with matching values
+    #find all of their values
+    count = 0
+    until count == 81
+      @board.each do |key, val|
+        require 'pry'
+        binding.pry
+        if @board[key] != 0
+          count += 1
+        else
+          peers = []
+          @board.each do |other_key, other_val|
+            peers << other_val if other_key.chars.any? {|spot| key.include?(spot) }
+          end
+          peers.delete_if {|num| num == 0}
+          if peers.uniq.length == 8
+            peers.uniq.sort.each_with_index do |num, index|
+              @board[key] = all_numbers[index] if num != all_numbers[index]
+            end
+          end
+          count += 1
+        end
       end
     end
-    final_positions = positions.flatten.zip(squares).map(&:join).map {|x| x = Cell.new}
-    final_positions.flatten.zip(split_data).to_h
   end
 
 
-  # def output_data
-  #   @puzzle_text
-  #   LETTERMAP
-  # end
 
-  # parse into row, column, square
 
-  # coordinate system with a hash.... A1, A2, A3... to map a board
+  # parse into to_be_replaced_by_rows, column, square
+
+  # coordinate system with a hash.... A1, A2, A3... to map a @board
 
   # know the value of a spot and/or if a spot is blank or not
-  # know where each spot is on the board
+  # know where each spot is on the @board
   # find all of a spot's peers...
   # check to see if there is an easy solution
   # if not move on to check next open spot
@@ -79,41 +130,3 @@ class Cell
 end
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-# def make_board
-#   letters = ("a".."i").to_a
-#   numbers = (1..9).to_a
-#   board = []
-#   index_counter = 0
-#   9.times do
-#     x = letters[index_counter]
-#     index_counter += 1
-#     row = [x,x,x,x,x,x,x,x,x]
-#     row.zip(numbers)
-#     board << row
-#   end
-#   p board
-# end
--- test/solver_test.rb
+++ test/solver_test.rb
@@ -1,10 +1,13 @@
 require "minitest"
 require "minitest/autorun"
+require 'minitest/pride'
 require "./lib/solver"
 # require "pride"
 # require "./lib/sudoku"
 
 class SolverTest < Minitest::Test
+
+  attr_reader :solver
 
   def setup
     puzzle_text = File.read("./puzzles/easy_sample.txt")
@@ -12,24 +15,31 @@ class SolverTest < Minitest::Test
   end
 
   def test_it_solves
+    skip
     assert false, "make it solve!"
   end
 
-  def test_it_can_output_puzzle_text
-    skip
-    assert_equal "", @solver.output_data
-  end
-
-  def test_it_can_split_data
-    skip
-    assert_equal "", @solver.split_data
+  def test_it_can_parse_the_puzzle_text
+    assert_equal [0, 2, 6, 5, 9, 4, 3, 1, 7, 7, 1, 5, 6,
+        3, 8, 9, 4, 2, 3, 9, 4, 7, 2, 1, 8, 6, 5, 1, 6, 3,
+        4, 5, 9, 2, 7, 8, 9, 4, 8, 2, 6, 7, 1, 5, 3, 2, 5,
+        7, 8, 1, 3, 6, 9, 4, 5, 3, 1, 9, 4, 2, 7, 8, 6, 4,
+        8, 2, 1, 7, 6, 5, 3, 9, 6, 7, 9, 3, 8, 5, 4, 2, 1],
+    solver.split_data
   end
 
   def test_it_makes_a_board
-    assert_equal "", @solver.make_board
-    assert_equal "", @solver.build_square_map_array
-
+    assert_equal 0, solver.make_board["a1z"]
+    assert_equal 6, solver.board["c1z"]
+    assert_equal 1, solver.board["i9r"]
   end
+
+  def test_it_can_find_peers_and_sub
+    skip
+    solver.find_peers_and_sub
+    assert_equal 8, solver.board["a1z"]
+  end
+
 
 
   # validation for solutions --- check row, column, and square
