eb5c6a14515042d3ce0455b0f686e3e26c1c5b7f
Morgan Miller <m.miller722@gmail.com>
2015-05-06 11:31:37 -0600

    this a mess that is solving things. refactoring to cell class

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,5 +1,6 @@
 require 'pry'
 
+
 class Solver
 
   attr_reader :board
@@ -8,8 +9,7 @@ class Solver
     @puzzle_text = puzzle_text
     @board = make_board
   end
-
-  ###Parser is chopping off spaces if they are at the end of the line
+  
   def split_data
     @puzzle_text.split("\n").map(&:chars).flatten.map(&:to_i)
   end
@@ -44,7 +44,7 @@ class Solver
     positions
   end
 
-  def shovel_squares(squares, first, second, third)
+  def order_squares(squares, first, second, third)
     3.times do
       squares << [first[0], first[0], first[0]]
       squares << [second[0], second[0], second[0]]
@@ -58,7 +58,7 @@ class Solver
     third = ["x","u","r"]
     squares = []
     3.times do
-      shovel_squares(squares, first, second, third)
+      order_squares(squares, first, second, third)
       first.rotate!; second.rotate!; third.rotate!
     end
     squares.flatten
@@ -76,30 +76,42 @@ class Solver
     peers.delete_if {|num| num == 0}.uniq
   end
 
-  def find_peer_cells(cell, num)
-    @board.keys.select { |other| cell[num] == other[num] unless other == cell }
+  def find_peer_cells(cell, index)
+    @board.keys.select { |other| cell[index] == other[index] unless other == cell }
   end
 
   def row_peers(cell)
+    find_peer_cells(cell, 1).map do |peer|
+      peer
+    end
+  end
+
+  def column_peers(cell)
+    find_peer_cells(cell, 0).map do |peer|
+      peer
+    end
+  end
+  
+  def row_peer_values(cell)
     find_peer_cells(cell, 1).map do |peer|
       @board[peer]
     end.delete_if {|num| num == 0}
   end
 
-  def column_peers(cell)
+  def column_peer_values(cell)
     find_peer_cells(cell, 0).map do |peer|
       @board[peer]
     end.delete_if {|num| num == 0}
   end
 
-  def square_peers(cell)
+  def square_peer_values(cell)
     find_peer_cells(cell, 2).map do |peer|
       @board[peer]
     end.delete_if {|num| num == 0}
   end
 
-  def all_peers(cell)
-    unique_peers(column_peers(cell) + square_peers(cell) + row_peers(cell))
+  def all_peer_values(cell)
+    unique_peers(column_peer_values(cell) + square_peer_values(cell) + row_peer_values(cell))
   end
 
   def empty_cells
@@ -107,19 +119,57 @@ class Solver
       empty_cells << cell if @board[cell] == 0 || @board[cell].nil?; empty_cells
     end
   end
+  
+  def empty_square_mates(designator)
+    @board.keys.select do |cell|
+      cell if cell[2] == designator && empty_cells.include?(cell)
+    end
+  end
+  
 
   def cell_to_solve
-    empty_cells.sort_by { |cell| all_peers(cell).length }.last
+    empty_cells.sort_by { |cell| all_peer_values(cell).length }.last
+    empty_cells.each do |cell|
+      puts "#{cell} has the following possibilities: #{possible_solutions(cell)}"
+    end
   end
 
   def all_possible_numbers
     (1..9).to_a
   end
-
-  def compare_numbers(cell)
-    all_possible_numbers.delete_if{ |num| all_peers(cell).include?(num) }
+  
+  # def account_for_pointing_peers(designator)
+  #   solns = empty_square_mates(designator).map do |cell|
+  #     possible_solutions(cell)
+  #   end
+  #   flappy = empty_square_mates(designator).zip(solns)
+  #   meh = flappy.flatten.reject {|val| val.is_a?(String)}.uniq
+  #   meh.each do |num|
+  #     sister_cells = []
+  #     flappy.each do |cell, values|
+  #       if values.include?(num)
+  #         sister_cells << cell
+  #       end
+  #     end
+  #     if sister_cells.length == 2 && row_peers(sister_cells[0]).include?(sister_cells[1]) || column_peers(sister_cells[0]).include?(sister_cells[1])
+  #       #####For this to work, cells need to be a class.
+  #     end
+  #   end
+  # # binding.pry
+  # end
+  
+  def possible_solutions_forreal(cell)
+    possible_solutions(cell)
   end
 
+  def possible_solutions(cell)
+    all_possible_numbers.delete_if{ |num| all_peer_values(cell).include?(num) }
+    #Remove pointing peers before returning possible solutions for cell
+  end
+
+  
+###Begin heuristic that checks peers of empty cells in same designator group
+###and solves cell if all other empty cells in the same group share a common peer
   def needed_numbers_for_group(designator)
     all_values = []
     @board.each do |cell, value|
@@ -140,19 +190,19 @@ class Solver
     all_cells.select { |cell| empty_cells.include?(cell) }
   end
 
+  ##There is a way better way to do this without a count, but I can't remember what it is...
   def check_peers(designator)
     needed = needed_numbers_for_group(designator)
     needed.length.times do
       count = 0
       all_empty_cells_for_group(designator).each do |cell|
-        if all_peers(cell).include?(needed.last)
+        if all_peer_values(cell).include?(needed.last)
           count +=1
         end
       end
-      # puts "count: #{count} \n comparator: #{all_empty_cells_for_group(designator).length-1}"
       if count == (all_empty_cells_for_group(designator).length - 1)
         solved_cell = all_empty_cells_for_group(designator).detect do |cell|
-          !all_peers(cell).include?(needed.last)
+          !all_peer_values(cell).include?(needed.last)
         end
         puts solved_cell
         @board[solved_cell] = needed.last
@@ -165,7 +215,6 @@ class Solver
     rows.each {|row| check_peers(row)}
     columns.each {|col| check_peers(col)}
     squares.each {|sq| check_peers(sq)}
-
   end
 
   def solved?
@@ -176,12 +225,16 @@ class Solver
     puts @board.values.select {|v| v==0}.length
     puts @board
     until solved?
-      if all_peers(cell_to_solve).length == 8
-        @board[cell_to_solve] = compare_numbers(cell_to_solve)[0]
+      if possible_solutions(cell_to_solve).length == 1
+        @board[cell_to_solve] = possible_solutions(cell_to_solve)[0]
       end
       check_them_all
       puts @board.values.select {|v| v==0}.length
-      # puts @board
     end
   end
 end
+
+
+puzzle_text = File.read("./puzzles/medium.txt")
+solver = Solver.new(puzzle_text)
+solver.account_for_pointing_peers('s')
--- test/solver_test.rb
+++ test/solver_test.rb
@@ -2,73 +2,73 @@ require "minitest"
 require "minitest/autorun"
 require 'minitest/pride'
 require "./lib/solver"
-# require "pride"
 # require "./lib/sudoku"
 
 class SolverTest < Minitest::Test
 
-  # def test_it_can_parse_the_puzzle_text
-  #   puzzle_text = File.read("./puzzles/easy_sample.txt")
-  #   solver = Solver.new(puzzle_text)
-  #
-  #   assert_equal [0, 2, 6, 5, 9, 4, 3, 1, 7, 7, 1, 5, 6,
-  #       3, 8, 9, 4, 2, 3, 9, 4, 7, 2, 1, 8, 6, 5, 1, 6, 3,
-  #       4, 5, 9, 2, 7, 8, 9, 4, 8, 2, 6, 7, 1, 5, 3, 2, 5,
-  #       7, 8, 1, 3, 6, 9, 4, 5, 3, 1, 9, 4, 2, 7, 8, 6, 4,
-  #       8, 2, 1, 7, 6, 5, 3, 9, 6, 7, 9, 3, 8, 5, 4, 2, 1],
-  #   solver.split_data
-  # end
-  #
-  # def test_it_makes_a_board
-  #   puzzle_text = File.read("./puzzles/easy_sample.txt")
-  #   solver = Solver.new(puzzle_text)
-  #
-  #   assert_equal 0, solver.board["a1z"]
-  #   assert_equal 6, solver.board["c1z"]
-  #   assert_equal 1, solver.board["i9r"]
-  # end
-  #
-  # def test_it_can_solve_easy_puzzle
-  #   puzzle_text = File.read("./puzzles/easy_sample.txt")
-  #   solver = Solver.new(puzzle_text)
-  #
-  #   solver.solve
-  #   assert_equal 8, solver.board["a1z"]
-  # end
-  #
-  # def test_it_can_find_row_peers
-  #   puzzle_text = File.read("./puzzles/easy_sample.txt")
-  #   solver = Solver.new(puzzle_text)
-  #
-  #   assert_equal [6, 5, 9, 4, 3, 1, 7], solver.row_peers("b1z")
-  # end
-  #
-  # def test_it_can_find_square_peers
-  #   puzzle_text = File.read("./puzzles/easy_sample.txt")
-  #   solver = Solver.new(puzzle_text)
-  #
-  #   assert_equal [6, 7, 1, 5, 3, 9, 4], solver.square_peers("b1z")
-  # end
-  #
-  # def test_it_can_find_column_peers
-  #   puzzle_text = File.read("./puzzles/easy_sample.txt")
-  #   solver = Solver.new(puzzle_text)
-  #
-  #   assert_equal [1, 9, 6, 4, 5, 3, 8, 7], solver.column_peers("b1z")
-  # end
-  #
-  # def test_it_finds_needed_numbers_for_row
-  #   puzzle_text = File.read("./puzzles/easy_sample.txt")
-  #   solver = Solver.new(puzzle_text)
-  #
-  #   assert_equal [8], solver.needed_numbers_for_group("a")
-  # end
-
-  def test_it_solves_easy_1
-    puzzle_text = File.read("./puzzles/solve_new.txt")
+  def test_it_can_parse_the_puzzle_text
+    puzzle_text = File.read("./puzzles/easy_sample.txt")
     solver = Solver.new(puzzle_text)
+  
+    assert_equal [0, 2, 6, 5, 9, 4, 3, 1, 7, 7, 1, 5, 6,
+        3, 8, 9, 4, 2, 3, 9, 4, 7, 2, 1, 8, 6, 5, 1, 6, 3,
+        4, 5, 9, 2, 7, 8, 9, 4, 8, 2, 6, 7, 1, 5, 3, 2, 5,
+        7, 8, 1, 3, 6, 9, 4, 5, 3, 1, 9, 4, 2, 7, 8, 6, 4,
+        8, 2, 1, 7, 6, 5, 3, 9, 6, 7, 9, 3, 8, 5, 4, 2, 1],
+    solver.split_data
+  end
+  
+  def test_it_makes_a_board
+    puzzle_text = File.read("./puzzles/easy_sample.txt")
+    solver = Solver.new(puzzle_text)
+  
+    assert_equal 0, solver.board["a1z"]
+    assert_equal 6, solver.board["c1z"]
+    assert_equal 1, solver.board["i9r"]
+  end
+  
+  def test_it_can_solve_easy_puzzle
+    puzzle_text = File.read("./puzzles/easy_sample.txt")
+    solver = Solver.new(puzzle_text)
+  
+    solver.solve
+    assert_equal 8, solver.board["a1z"]
+  end
+  
+  def test_it_can_find_row_peer_values
+    puzzle_text = File.read("./puzzles/easy_sample.txt")
+    solver = Solver.new(puzzle_text)
+  
+    assert_equal [6, 5, 9, 4, 3, 1, 7], solver.row_peer_values("b1z")
+  end
+  
+  def test_it_can_find_square_peer_values
+    puzzle_text = File.read("./puzzles/easy_sample.txt")
+    solver = Solver.new(puzzle_text)
+  
+    assert_equal [6, 7, 1, 5, 3, 9, 4], solver.square_peer_values("b1z")
+  end
+  
+  def test_it_can_find_column_peer_values
+    puzzle_text = File.read("./puzzles/easy_sample.txt")
+    solver = Solver.new(puzzle_text)
+  
+    assert_equal [1, 9, 6, 4, 5, 3, 8, 7], solver.column_peer_values("b1z")
+  end
+  
+  def test_it_finds_needed_numbers_for_row
+    puzzle_text = File.read("./puzzles/easy_sample.txt")
+    solver = Solver.new(puzzle_text)
+  
+    assert_equal [8], solver.needed_numbers_for_group("a")
+  end
+  
+  def test_it_solves_easy_1
+    puzzle_text = File.read("./puzzles/medium.txt")
+    solver = Solver.new(puzzle_text)
+    solver.solve
 
-    assert_equal [], solver.solve
+    assert_equal [], solver.board
   end
 
 
